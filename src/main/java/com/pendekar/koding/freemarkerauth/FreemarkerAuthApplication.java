adbd97ce99d7b15b895ff6961bb7e031151ca3b9
rizqi_tanto <rizqi_tanto@datascrip.co.id>
2025-05-19 23:31:12 +0700

    first commit

---
+++ src/main/java/com/pendekar/koding/freemarkerauth/FreemarkerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.pendekar.koding.freemarkerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerAuthApplication.class, args);
+    }
+
+}
